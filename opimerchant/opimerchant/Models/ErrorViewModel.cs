3d830274c307c9e15969bed1034f46abead67ef5
Apeirophobia <ervinpusijainen@gmail.com>
2026-04-01 10:49:29 +0300

    init

---
+++ opimerchant/opimerchant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace opimerchant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
