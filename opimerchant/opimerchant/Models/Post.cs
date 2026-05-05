b612b6e7ae24209812d586adf2cb73dfa80fed09
Apeirophobia <ervinpusijainen@gmail.com>
2026-05-05 10:50:18 +0300

    Updated Post model

--- opimerchant/opimerchant/Models/Post.cs
+++ opimerchant/opimerchant/Models/Post.cs
@@ -5,7 +5,7 @@ namespace opimerchant.Models
     public class Post
     {
         [Key]
-        public Guid ID { get; set; }
+        public Guid PostID { get; set; }
         [Required]
         public string Title { get; set; }
         public string Description { get; set; }
