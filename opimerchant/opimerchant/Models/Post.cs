ac0460cd29841b7aa36972064e108a762215e4e4
Apeirophobia <ervinpusijainen@gmail.com>
2026-05-08 13:08:44 +0300

    Updated Post Model
    Added Author field

--- opimerchant/opimerchant/Models/Post.cs
+++ opimerchant/opimerchant/Models/Post.cs
@@ -8,6 +8,7 @@ namespace opimerchant.Models
         public Guid PostID { get; set; }
         [Required]
         public string Title { get; set; }
+        public string Author { get; set; }
         public string Description { get; set; }
         [Required]
         public string Body { get; set; }
