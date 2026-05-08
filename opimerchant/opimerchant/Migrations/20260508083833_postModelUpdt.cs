2bb4e1d1fed6e7ccd355da7e4d89bdb6162b786f
Apeirophobia <ervinpusijainen@gmail.com>
2026-05-08 13:07:49 +0300

    Immigration

---
+++ opimerchant/opimerchant/Migrations/20260508083833_postModelUpdt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace opimerchant.Migrations
+{
+    /// <inheritdoc />
+    public partial class postModelUpdt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Posts");
+        }
+    }
+}
