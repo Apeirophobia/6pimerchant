97410cff05344f26fe1b85552bbdc23671a94b0a
Apeirophobia <ervinpusijainen@gmail.com>
2026-05-08 15:28:31 +0300

    Easter Eggs :D

---
+++ opimerchant/opimerchant/Controllers/SecretController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace opimerchant.Controllers
+{
+    public class SecretController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GrassBlock()
+        {
+
+            return View();
+        }
+
+        public IActionResult Cow()
+        {
+
+            return View();
+        }
+    }
+}
